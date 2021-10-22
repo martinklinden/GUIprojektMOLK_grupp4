8ce26fa6632701bebeab9345a45c2743db8cbc1b
Martin Lindén <martin.linden.k@gmail.com>
2021-10-22 14:44:52 +0200

    exe-test working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +26,29 @@ namespace GUIprojektMOLK_grupp4
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process process = new Process();
+            
+
+            string path = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Molk";
+            var startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WorkingDirectory = path,
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                RedirectStandardInput = true,
+                UseShellExecute = false
+            };
+
+            process.StartInfo = startInfo;
+            process.Start();
+
+            process.StandardInput.WriteLine($"molk -j {destinationBox.Text}\\archive2.molk {fileBox.Text}");
+
+            //molk path+archivename path+fileORfiletyp
+        }
+
     }
 }
